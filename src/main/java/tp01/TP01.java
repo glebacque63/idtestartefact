643e1b7cb6e281ae15a1dbddb5035822305cb79c
glebacque63 <guillaume.lebacque.tic@gmail.com>
2018-11-07 14:35:37 +0100

    MAJ
    on s'en moque

---
+++ src/main/java/tp01/TP01.java
@@ -0,0 +1,16 @@
+package tp01;
+
+public class TP01 {
+
+	public static void main(String[] args) {
+		System.out.println("Hello, World !");
+		int a = multiplier(2, 3);
+		System.out.println(a);
+		
+	}
+	
+	public static int multiplier(int a, int b){
+		return a*b;
+	}
+
+}
